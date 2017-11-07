8015450ef4c57ccf88478691c2623f63971877fe
Brian <bjudaprawira@horizon.csueastbay.edu>
2017-11-06 16:57:18 -0800

    added readNameAndRespond functionality

---
+++ php/getMyNodeResults.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'HTTP/Request2.php';
+
+$request = new HTTP_Request2('https://csweb01.csueastbay.edu/~ae4654/exercise3/readNameAndRespond');
+$request->setMethod(HTTP_Request2::METHOD_POST)
+    ->addPostParameter('name', 'CS3520');
+
+// ######### To Fix the SSL issue ###########
+$request->setConfig(array(
+    'ssl_verify_peer'   => FALSE,
+    'ssl_verify_host'   => FALSE
+));
+// ########################################
+
+//invoke request and get the response
+try {
+    $response = $request->send();
+    if (200 == $response->getStatus()) {
+        echo $response->getBody();
+    } else {
+        echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+            $response->getReasonPhrase();
+    }
+} catch (HTTP_Request2_Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+}
+
+?>
