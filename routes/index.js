51070eccda50a356d15fc3fe5185faddc259c3ad
Brian <bjudaprawira@horizon.csueastbay.edu>
2017-11-14 21:11:36 -0800

    getAllOrders 6

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ var controllerMongoCollection = require('../controllers/database'); //load contr
 
 //########################################
 //to process data sent in on request need body-parser module
-var bodyParser = require('body-parser');
+/*var bodyParser = require('body-parser');
 var path = require ('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
 
 router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+router.use(bodyParser.urlencoded({ extended: true }));*/ // for parsing application/x-www-form-urlencode
 //#########################################
 
 router.get('/mongodb', function (request, response) {
@@ -45,15 +45,15 @@ router.get('/mongodb', function (request, response) {
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 //now processing post
-router.post('/readNameAndRespond', function(req, res, next) {
+/*router.post('/readNameAndRespond', function(req, res, next) {
     //expecting data variable called name --retrieve value using body-parser
     var value_name = req.body.name;  //retrieve the data associated with name
     res.send("hello " + value_name);
-});
+});*/
 
  //GET home page.
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 module.exports = router;
