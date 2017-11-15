c5ff5d9614930024f97575aad5e5fe6d6ba96755
Brian <bjudaprawira@horizon.csueastbay.edu>
2017-11-14 21:06:30 -0800

    getallOrders 5

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://newUser:brave123@ds259105.mlab.com:59105/heroku_6rdr5zwb';
 //LOAD the various controllers
 //var controllerMain = require('../controllers/main');   //this will load the main controller file
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
@@ -14,6 +15,32 @@ var querystring = require('querystring'); //for use in GET Query string of form 
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
+
+router.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+
+        //get collection of orders
+        var Orders = db.collection('Orders');
+
+        //get all Orders
+        Orders.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+
+            if(err) throw err;
+
+            response.render('/getAllOrders', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+
+    });//end of connect
+
+});//end router.get
 
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
